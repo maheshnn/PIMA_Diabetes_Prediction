42dd742f712733a6c7fa0b83588bbd2fb164691d
mahes <mahesh.narayananhere@gmail.com>
2026-02-27 17:09:57 -0600

    fix: Data upload

--- self_paced_courses_1_mlops/model_building/data_register.py
+++ self_paced_courses_1_mlops/model_building/data_register.py
@@ -1,24 +1,25 @@
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
-from huggingface_hub import HfApi, create_repo
+from huggingface_hub import HfApi, create_repo, login
 import os
 
 repo_id = "maheshnn/PIMA-Diabetes-Prediction" 
 repo_type = "dataset"
 
 # Initialize API client
-api = HfApi(token=os.getenv("HF_TOKEN"))
+login(os.getenv("HF_TOKEN"))
+api = HfApi()
 
 # Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
+    print(f"Dataset '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
+    print(f"Dataset '{repo_id}' not found. Creating new space...")
+    api.create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Dataset '{repo_id}' created.")
 
 api.upload_folder(
-    folder_path="PIMA_Diabetes_Prediction/self_paced_courses_1_mlops/data", # Uploading the data 
+    folder_path="self_paced_courses_1_mlops/data", # Uploading the data 
     repo_id=repo_id,
     repo_type=repo_type,
 )
